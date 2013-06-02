c4479c21b7e694855346d85896d2eca8b4be10a1
Jim Lake <jim@blueskylabs.com>
2013-06-02 16:36:08 -0700

    framework

--- cloudflow-server/routes/api/index.js
+++ cloudflow-server/routes/api/index.js
@@ -1,9 +1,12 @@
 
-var data_app = require('./applications');
+var api_app = require('./applications');
+var api_env = require('./environments');
 
-exports.addRoutes = function(app,prefix) {
+exports.addRoutes = function(app,prefix)
+{
+    app.get(prefix + '/applications',api_app.get);
+    app.post(prefix + '/applications',api_app.post);
 
-    app.get(prefix + '/applications',data_app.get);
-    app.post(prefix + '/applications',data_app.post);
-
+    app.get(prefix + '/environments',api_env.get);
+    app.post(prefix + '/environments',api_env.post);
 };
--- cloudflow-server/static/js/home.js
+++ cloudflow-server/static/js/home.js
@@ -1,5 +1,43 @@
 
+var g_applications = [];
+var g_environments = [];
+
 function indexReady()
 {
+    jQuery.ajax(
+    {
+        type: 'GET',
+        url: '/api/applications',
+        dataType: 'json',
+        success: function(data) 
+        {
+            g_applications = data;
+            renderIndex();
+        },
+        error: function()
+        {
+            window.alert("Failed to get application data.");
+        }
+    });
+    jQuery.ajax(
+    {
+        type: 'GET',
+        url: '/api/environments',
+        dataType: 'json',
+        success: function(data) 
+        {
+            g_environments = data;
+            renderIndex();
+        },
+        error: function()
+        {
+            window.alert("Failed to get environment data.");
+        }
+    });
 }
 $(document).ready(indexReady);
+
+
+function renderIndex()
+{
+}
